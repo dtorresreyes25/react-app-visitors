1179bf76e468afba6e15419ae0d421f344e7e5a5
Dayron R <dtorresreyes25@gmail.com>
2019-09-10 10:31:39 -0400

    interfaz y nuevos modulos

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from "./components/header_footer/Header";
 import Grid from "@material-ui/core/Grid";
 import Footer from "./components/header_footer/Footer";
 import SignInSide from "./components/SignInSide/SignInSide";
+import HomeForm from "./components/Form";
 import "./resources/styles.css";
 
 function App() {
   return (
     <div>
-      <SignInSide />
+      {/* <SignInSide /> */}
+      <HomeForm />
     </div>
   );
 }
