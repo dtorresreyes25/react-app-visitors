9fba6f7c5a1dd28f06ef50914199220faf8f9151
Dayron R <dtorresreyes25@gmail.com>
2019-11-05 13:45:44 -0500

    new split component added

---
+++ src/components/sign_in/signInForm.js
@@ -0,0 +1,71 @@
+import React, { useState} from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Logo from "../../resources/logo-ict.png";
+import Copyright from '../header_footer/copyright'
+import {signInStyles} from '../ui/styles'
+
+const SignInForm = ({handleChange,errors,values,handleSubmit,}) => {
+
+  const classes = signInStyles();
+  return (
+     <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={8} className={classes.image} />
+      <Grid item xs={12} sm={8} md={4} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+            <img src={Logo} alt="logo" className={classes.logo} />         
+          <form className={classes.form} onSubmit={handleSubmit}>
+            
+            <TextField
+                  variant="outlined"
+                  margin="normal"
+                  error={errors.email && true}
+                  fullWidth
+                  id="email"
+                  label="email"
+                  name="email"
+                  value={values.email || ""}
+                  autoComplete="email"
+                  autoFocus
+                  helperText={errors.email}
+                  onChange={handleChange}
+            />
+            <TextField
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                  error={errors.password && true}
+                  helperText={errors.password}
+                  name="password"
+                  label="Contraseña"
+                  type="password"
+                  value={values.password || ""}
+                  id="password"
+                  autoComplete="current-password"
+                  onChange={handleChange}
+            />
+            <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+            >
+              Entrar
+            </Button>   
+            <Box mt={5}>  
+              <Copyright />
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default SignInForm;
